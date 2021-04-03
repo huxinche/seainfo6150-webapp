faca6b736e9c1745ffdee88de3000800172c38f8
huxinche <hu.xinc@northeastern.edu>
2021-04-03 15:00:46 -0400

    updating responsive layout assignment

--- src/ArticleListItem/ArticleListItem.jsx
+++ src/ArticleListItem/ArticleListItem.jsx
@@ -9,14 +9,16 @@ const ArticleListItem = (props) => {
   const [show, setShow] = useState(false)
   return (
     <div className={styles.card}>
-      <ArticleImage url={props.article.image._url} title={props.article.title}/>
-      <Link to={`/articlelist/${props.slug}`} className={styles.title}>{props.article.title}</Link>
-      {show && <>
+      <div className={styles.main}>
+        <ArticleImage url={props.article.image._url} title={props.article.title}/>
+        <Link to={`/articlelist/${props.slug}`} className={styles.title}>{props.article.title}</Link>
+      </div>
+      <div className={styles.info}>{show && <>
         <time dateTime={props.article.timeStamp} className={styles.time}>
           {props.article.displayDate}
         </time>
         <p className={styles.text}>{props.article.shortText}</p>
-      </>}
+      </>}</div>
       <ArticleTextToggleButton buttonText={show ? 'Show less' : 'Show more'} onClick={() => setShow(!show)}/>
     </div>
   );
